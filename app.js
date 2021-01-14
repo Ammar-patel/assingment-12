c25338e3f33d4973bc5a69ab0bf7d037e5bd8f56
Ammar-patel <75995089+Ammar-patel@users.noreply.github.com>
2021-01-14 20:49:12 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,45 @@
+// var name = "John Doe"
+// var prod = "T-shirts(s)"
+// var qty = "5"
+// document.write(name.bold() + " ordered " + qty.bold() + prod.bold() + " on XYZ Clothing store.");
+
+// document.write ("<b>Rules for naming JS variables</b> <br><br><br> Variable names can only contain, numbers, $ and _. For example : $my_1stVariable <br> Variable must begin with a letter, $ or _ .For example : $name, _name or name <br> Variable names are case sensitive <br> Variable names should not be JS Keywords");
+
+// var a = 3;
+// var b = 5;
+// document.write("Sum of 3 and 5 is " + (a+b));
+// document.write("<br> Subtraction of 3 and 5 is " + (a-b));
+// document.write("<br> Multplication of 3 and 5 is " + (a*b));
+// document.write("<br> division of 3 and 5 is " + (a/b));
+// document.write("<br> Modulas of 3 and 5 is " + (a%b));
+
+// var exp = 17;
+// document.write("Value after variable declaration is: " + exp);
+
+var int123;
+int123 = 5
+// document.write("Initialvalue: "+int123);
+
+// document.write(int123++);
+
+// document.write(++int123); 
+// int123= int123 + 7;
+// document.write("<br>" + "Value after addition is: " + int123);
+// // document.write("<br>"+ int123--);
+// document.write("<br>"+ (--int123));
+// document.write("<br>"+"Value after decrement is: " +int123)
+// document.write("<br>The remainder is : " + int123 % 3);
+
+// var tic = 600;
+
+
+var prom1 = +prompt("enter number");
+var prom2 = +prompt("enter number to start");
+var prom3 = +prompt("enter number to end");
+for (var prom2; prom2 <= prom3; prom2++) {
+    console.log(prom1 + "x" + prom2 + "="+ prom1 * prom2 );
+}
+// for (var i = 1; i <= 10; i++){
+//     document.write("2" + "x" + "i" + "=" + 2 * i + "<br />");
+// }
+
